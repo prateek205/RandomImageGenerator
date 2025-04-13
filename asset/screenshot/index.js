dd17882fb253e9de9aa615a915e1930763d56acf
Prateek_Bahad <150145430+prateek205@users.noreply.github.com>
2025-04-13 22:09:50 +0530

    Add files via upload

---
+++ asset/screenshot/index.js
@@ -0,0 +1,18 @@
+// console.log("object");
+
+const button = document.querySelector("button");
+
+function getRandomImages() {
+  const img = Math.random() * 1000;
+  document.getElementById(
+    "picsumImage"
+  ).src = `https://picsum.photos/1024/500?${img}`;
+}
+
+button.addEventListener('click', ()=>{
+    getRandomImages()
+})
+
+// setInterval(() => {
+//   getRandomImages();
+// }, 3000);
